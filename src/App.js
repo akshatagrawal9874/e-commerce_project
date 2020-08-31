f0441d06c6213c6370726f188f71b12ddec7ea45
Akshatagrawal98 <18100BTCSE02657@svvvindore.onmicrosoft.com>
2020-08-31 11:08:15 +0530

    Components using redux

--- src/App.js
+++ src/App.js
@@ -70,3 +70,4 @@ const mapDispatchToProps =dispatch =>({
   setCurrentUser:user =>dispatch(setCurrentUser(user))
 })
 export default connect(mapStateToProps,mapDispatchToProps)(App);
+//reducer in redux
